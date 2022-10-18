5ab55f95818d786be45287f88289d27e3e430676
jey707 <jey707@naver.com>
2022-10-18 21:49:57 +0900

    react-hooks practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { type } from "@testing-library/user-event/dist/type";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 //usestate
 /** function App() {
@@ -45,7 +45,7 @@ function App() {
 } */
 
 //useTabs
-const content = [
+/** const content = [
   { tab: "Section 1 ", content: "I'm the content of the Section 1" },
   { tab: "Section 2 ", content: "I'm the content of the Section 2" },
 ];
@@ -69,6 +69,94 @@ function App() {
       <div>{currentItem.content}</div>
     </div>
   );
-}
+} */
 
+//useTitle
+/** const useTitle = (inititialTitle) => {
+  const [title, setTitle] = useState(inititialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 3000);
+  return <div className="App"></div>;
+} */
+
+// useClick
+/** const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListner("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+function App() {
+  const sayHello = () => console.log("say Hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+} */
+
+// useConfirm
+/** const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  if (typeof rejection !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+function App() {
+  const deleteWorld = () => console.log("delete world...");
+  const about = () => console.log("Abouted");
+  const confirmDelete = useConfirm("Atr you sure", deleteWorld, about);
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+} */
+
+// usePreventLeave
+const usepreventLeave = () => {
+  const listener = (event) => {
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+function App() {
+  const { enablePrevent, disablePrevent } = usepreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
+    </div>
+  );
+}
 export default App;
