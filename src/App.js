2ea3eed734903ee9ebc27a542ef5bc10096136fb
jey707 <jey707@naver.com>
2022-10-18 19:02:28 +0900

    react_hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { type } from "@testing-library/user-event/dist/type";
+import { useState } from "react";
 
-function App() {
+//usestate
+/** function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Item : {item}</h1>
+      <button onClick={incrementItem}>IncrementItem</button>
+      <button onClick={decrementItem}>DecrementItem</button>
+    </div>
+  );
+} */
+
+//useInput
+/** const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+function App() {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Hi", maxLen);
+  return (
+    <div className="App">
+      <h1>Input</h1>
+      <input placeholder="hahahaha" {...name} />
+    </div>
+  );
+} */
+
+//useTabs
+const content = [
+  { tab: "Section 1 ", content: "I'm the content of the Section 1" },
+  { tab: "Section 2 ", content: "I'm the content of the Section 2" },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setcurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return { currentItem: allTabs[currentIndex], changeItem: setcurrentIndex };
+};
+function App() {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)} key={index}>
+          {section.tab}
+        </button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
